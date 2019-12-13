b3d72f7191b17fd01356e8dfa2cb7afe3bb8e3d1
abrahamvee <49734112+abrahamvee@users.noreply.github.com>
2019-12-13 17:46:03 +0100

    Update balloon_mgr.c

--- balloon_mgr.c
+++ balloon_mgr.c
@@ -5,15 +5,17 @@
 #include <stdbool.h>
 
 extern void DoSendBalloon(void) ;
+extern SemaphoreHandle_t semBalloon;
 
 
 static unsigned int BalloonNeeded=0 ;
 static bool finished_updating = false;
 
 void UpdateBalloonNeeded(uint8_t const _BalloonNeeded)
-{
+{  
+       xSemaphoreTake(semBaloon,portMAX_DELAY);
    	 BalloonNeeded =_BalloonNeeded ;
-
+       xSemaphoreGive(semBalloon);
 }
 
 
@@ -25,13 +27,16 @@ void SendBalloon(void *argument)
 {
     while(1)
     {
+       xSemaphoreTake(semBaloon,portMAX_DELAY);
+       
    	 if (BalloonNeeded>0)
    	 {
    		 DoSendBalloon();
    		 BalloonNeeded-- ;
    	 }
+        xSemaphoreGive(semBalloon);
    	 osDelay(10);
-
+   
     }
 }
 
